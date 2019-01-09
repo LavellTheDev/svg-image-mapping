1d4e3d02c6a435f8e26aebaee39634127e262cec
Lavell Wesley <lavellthedev+github@gmail.com>
2019-01-09 13:49:08 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,156 @@
+// function toggleFunction() {
+//   $("#div1").toggleClass("display-none");
+//   $("#div2").toggleClass("display-inline");
+// }
+// function toggleBackBay(){
+//   $("#backbayCard").toggleClass("display-none");
+//   $("#backbayCard").toggleClass("display-inline");
+// }
+//
+// function toggleBeaconHill(){
+//   $("#beaconhillCard").toggleClass("display-none");
+//   $("#beaconhillCard").toggleClass("display-inline");
+// }
+// function toggleCharles(){
+//   $("#charlestownCard").toggleClass("display-none");
+//   $("#charlestownCard").toggleClass("display-inline");
+// }
+//
+// function toggleCambridge(){
+//   $("#cambridgeCard").toggleClass("display-none");
+//   $("#cambridgeCard").toggleClass("display-inline");
+// }
+//
+// function toggleDowntown(){
+//   $("#downtownCard").toggleClass("display-none");
+//   $("#downtownCard").toggleClass("display-inline");
+// }
+// function toggleDorchester(){
+//   $("#dorchesterCard").toggleClass("display-none");
+//   $("#dorchesterCard").toggleClass("display-inline");
+// }
+// function toggleEastBoston(){
+// $("#eastbostonCard").toggleClass("display-none");
+// $("#eastbostonCard").toggleClass("display-inline");
+// }
+// function toggleFenway(){
+// $("#fenwayCard").toggleClass("display-none");
+// $("#fenwayCard").toggleClass("display-inline");
+// }
+// function toggleJamaicaPlain(){
+//   $("#jamaicaplainCard").toggleClass("display-none");
+//   $("#jamaicaplainCard").toggleClass("display-inline");
+// }
+// function toggleLeatherDistrict(){
+//   $("#leatherdistrictCard").toggleClass("display-none");
+//   $("#leatherdistrictCard").toggleClass("display-inline");
+// }
+// function toggleNorthEnd(){
+//   $("#northendCard").toggleClass("display-none");
+//   $("#northendCard").toggleClass("display-inline");
+// }
+// function toggleRoxbury(){
+//   $("#roxburyCard").toggleClass("display-none");
+//   $("#roxburyCard").toggleClass("display-inline");
+// }
+// function toggleSeaport(){
+//   $("#seaportCard").toggleClass("display-none");
+//   $("#seaportCard").toggleClass("display-inline");
+// }
+// function toggleSouthBoston(){
+//   $("#seaportCard").toggleClass("display-none");
+//   $("#seaportCard").toggleClass("display-inline");
+// }
+// function toggleSoutEnd(){
+//   $("#southEndCard").toggleClass("display-none");
+//   $("#southEndCard").toggleClass("display-inline");
+// }
+// function toggleWestEnd(){
+//   $("#westEndCard").toggleClass("display-none");
+//   $("#westEndCard").toggleClass("display-inline");
+// }
+// function showImage(){
+// $(".clickableImage").toggleClass("display-none");
+// $(".clickableImage").toggleClass("display-inline");
+// }
+// function toggleClickable(){
+//   $(".clickableImage").toggleClass("display-inline")
+//   $(".clickableImage").toggleClass("display-none");
+// }
+// $('#toggleClickable').click(function() {
+//     $('#toggleClickable').hide();
+// });
+// var maps= document.getElementsByClassName('charlestown-toggle');
+// $(document).ready(function(){
+//
+//   $('.backbay-toggle').on('click', function(){
+//       $('.backbayCard').fadeToggle(1000);
+//   })
+//
+//
+//   $('.beaconhill-toggle').on('click', function(){
+//       $('.beaconhillCard').fadetoggle(1000);
+//   })
+//
+//   $('.cambridge-toggle').on('click', function(){
+//       $('.cambridgeCard').fadeToggle(1000);
+//   })
+//
+//   $('.charlestown-toggle').on('click', function(){
+//       $('.charlestownCard').fadeToggle(1000);
+//   })
+//
+//   $('.dorchester-toggle').on('click', function(){
+//       $('.dorchesterCard').fadeToggle(1000);
+//   })
+//
+//   $('.downtown-toggle').on('click', function(){
+//       $('.downtownCard').fadeToggle(1000);
+//   })
+//
+//   $('.eastboston-toggle').on('click', function(){
+//       $('.eastbostonCard').fadeToggle(1000);
+//   })
+//
+//   $('.fenway-toggle').on('click', function(){
+//       $('.fenwayCard').fadeToggle(1000);
+//   })
+//
+//   $('.jp-toggle').on('click', function(){
+//       $('.jamaicaplainCard').fadeToggle(1000);
+//   })
+//
+//   $('.leatherdistrict-toggle').on('click', function(){
+//       $('.leatherdistrictCard').fadeToggle(1000);
+//   })
+//
+//   $('.northend-toggle').on('click', function(){
+//       $('.northendCard').fadeToggle(1000);
+//   })
+//
+//   $('.roxbury-toggle').on('click', function(){
+//       $('.roxburyCard').fadeToggle(1000);
+//   })
+//
+//   $('.seaport-toggle').on('click', function(){
+//       $('.seaportCard').fadeToggle(1000);
+//   })
+//   $('.southboston-toggle').on('click', function(){
+//       $('.southBostonCard').fadeToggle(1000);
+//   })
+//   $('.southend-toggle').on('click', function(){
+//       $('.southEndCard').fadeToggle(1000);
+//   })
+//
+//   $('.westend-toggle').on('click', function(){
+//       $('.westEndCard').fadeToggle(1000);
+//   })
+// });
+// $(document).ready(function(){
+//   $('.charlestown-toggle').on('click', function(){
+//     $('.charlestownCard').toggle();
+//   })
+// });
+// element.click(function(){
+//   this.fill({ color: '#f06'})
+// });
