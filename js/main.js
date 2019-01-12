d1175549e5cbdf5de681a6bfebde54ac863f4c90
Lavell Wesley <lavellthedev+github@gmail.com>
2019-01-12 02:45:48 -0500

    hide and toggle is working, small problem with the display

--- js/main.js
+++ js/main.js
@@ -1,82 +1,13 @@
-// function toggleFunction() {
-//   $("#div1").toggleClass("display-none");
-//   $("#div2").toggleClass("display-inline");
-// }
-// function toggleBackBay(){
-//   $("#backbayCard").toggleClass("display-none");
-//   $("#backbayCard").toggleClass("display-inline");
-// }
-//
-// function toggleBeaconHill(){
-//   $("#beaconhillCard").toggleClass("display-none");
-//   $("#beaconhillCard").toggleClass("display-inline");
-// }
-// function toggleCharles(){
-//   $("#charlestownCard").toggleClass("display-none");
-//   $("#charlestownCard").toggleClass("display-inline");
-// }
-//
-// function toggleCambridge(){
-//   $("#cambridgeCard").toggleClass("display-none");
-//   $("#cambridgeCard").toggleClass("display-inline");
-// }
-//
-// function toggleDowntown(){
-//   $("#downtownCard").toggleClass("display-none");
-//   $("#downtownCard").toggleClass("display-inline");
-// }
-// function toggleDorchester(){
-//   $("#dorchesterCard").toggleClass("display-none");
-//   $("#dorchesterCard").toggleClass("display-inline");
-// }
-// function toggleEastBoston(){
-// $("#eastbostonCard").toggleClass("display-none");
-// $("#eastbostonCard").toggleClass("display-inline");
-// }
-// function toggleFenway(){
-// $("#fenwayCard").toggleClass("display-none");
-// $("#fenwayCard").toggleClass("display-inline");
-// }
-// function toggleJamaicaPlain(){
-//   $("#jamaicaplainCard").toggleClass("display-none");
-//   $("#jamaicaplainCard").toggleClass("display-inline");
-// }
-// function toggleLeatherDistrict(){
-//   $("#leatherdistrictCard").toggleClass("display-none");
-//   $("#leatherdistrictCard").toggleClass("display-inline");
-// }
-// function toggleNorthEnd(){
-//   $("#northendCard").toggleClass("display-none");
-//   $("#northendCard").toggleClass("display-inline");
-// }
-// function toggleRoxbury(){
-//   $("#roxburyCard").toggleClass("display-none");
-//   $("#roxburyCard").toggleClass("display-inline");
-// }
-// function toggleSeaport(){
-//   $("#seaportCard").toggleClass("display-none");
-//   $("#seaportCard").toggleClass("display-inline");
-// }
-// function toggleSouthBoston(){
-//   $("#seaportCard").toggleClass("display-none");
-//   $("#seaportCard").toggleClass("display-inline");
-// }
-// function toggleSoutEnd(){
-//   $("#southEndCard").toggleClass("display-none");
-//   $("#southEndCard").toggleClass("display-inline");
-// }
-// function toggleWestEnd(){
-//   $("#westEndCard").toggleClass("display-none");
-//   $("#westEndCard").toggleClass("display-inline");
-// }
-// function showImage(){
-// $(".clickableImage").toggleClass("display-none");
-// $(".clickableImage").toggleClass("display-inline");
-// }
-// function toggleClickable(){
-//   $(".clickableImage").toggleClass("display-inline")
-//   $(".clickableImage").toggleClass("display-none");
-// }
-// $('#toggleClickable').click(function() {
-//     $('#toggleClickable').hide();
-// });
+var hideImages = document.querySelectorAll('.clickableImage');
+
+ // hideImages[0].classList.add('hidden');
+
+function toggleBeaconHill(){
+  var x = document.getElementById('beaconhillCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+  console.log('toggleBeacon')
+}
