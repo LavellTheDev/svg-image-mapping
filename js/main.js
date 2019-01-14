3e3684570f87baaee065d6e70daf63555b3bb23c
Lavell Wesley <lavellthedev+github@gmail.com>
2019-01-14 01:45:36 -0500

    hide and toggle is working, small problem with the display

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var hideImages = document.querySelectorAll('.clickableImage');
 
- // hideImages[0].classList.add('hidden');
+ hideImages[0].classList.add('hidden');
 
 function toggleBeaconHill(){
   var x = document.getElementById('beaconhillCard');
   if (x.style.display === "none") {
-    x.style.display = "block"
+    x.style.display = "inline"
   }else{
     x.style.display = "none";
   }
