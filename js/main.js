8c5fe7bc6a269499adb333871161538477f7068a
Lavell Wesley <lavellthedev+github@gmail.com>
2019-01-15 12:22:46 -0500

    SVG is working, need to come back and optimize

--- js/main.js
+++ js/main.js
@@ -9,10 +9,137 @@
    }else{
      x.style.display = "none";
    }
+   // x.style.transition =  
  }
 
 function toggleBeaconHill(){
   var x = document.getElementById('beaconhillCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleCambridge(){
+  var x = document.getElementById('cambridgeCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleCharles(){
+  var x = document.getElementById('charlestownCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleDowntown(){
+  var x = document.getElementById('downtownCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleDorchester(){
+  var x = document.getElementById('dorchesterCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleEastBoston(){
+  var x = document.getElementById('eastbostonCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleFenway(){
+  var x = document.getElementById('fenwayCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleJamaicaPlain(){
+  var x = document.getElementById('jamaicaplainCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleLeatherDistrict(){
+  var x = document.getElementById('leatherdistrictCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleNorthEnd(){
+  var x = document.getElementById('northendCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleRoxbury(){
+  var x = document.getElementById('roxburyCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleSeaport(){
+  var x = document.getElementById('seaportCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleSouthBoston(){
+  var x = document.getElementById('southbostonCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleSoutEnd(){
+  var x = document.getElementById('southendCard');
+  if (x.style.display === "none") {
+    x.style.display = "block"
+  }else{
+    x.style.display = "none";
+  }
+}
+
+function toggleWestEnd(){
+  var x = document.getElementById('westendCard');
   if (x.style.display === "none") {
     x.style.display = "block"
   }else{
